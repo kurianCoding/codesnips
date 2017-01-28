e0c11efbc05cf3007763ada04db08bc26cf9b1c2
kurian <kurianck.mail@gmail.com>
2017-01-28 11:53:14 +0530

    GDG Bored
    This is what i did when i was bored in a GDG talk.

--- code/switch.c
+++ code/switch.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 int main(int argc,char *argv[]){
-	int choice=argc;
-	switch(choice){
-		case 2:
-			printf("damn world");
-			break;
+}
 
-		case 3:
-			printf("fuck world");
-			break;
+int predictX(int inputX,int velX){
+	return x;
+}
 
-		case 1:
-			printf("hello world");
-			break;
-		default:
-			printf("default");
-	}
-	return 0;
+int predictY(int inputY, int velY){
+	return y;
+}
+
+int regression(int actual, int requiredoutput){
+	return regression;
 }
